30bce9764bf1c2920ba81bb2cfc10993569245d3
cezarmathe <me@cezarmathe.com>
2020-05-23 18:58:18 +0300

    Create DocumentPdfFileRepository

--- src/main/java/com/code4ro/legalconsultation/repository/DocumentPdfFileRepository.java
+++ src/main/java/com/code4ro/legalconsultation/repository/DocumentPdfFileRepository.java
@@ -1,6 +1,7 @@
 package com.code4ro.legalconsultation.repository;
 
 import com.code4ro.legalconsultation.model.persistence.DocumentPdfFile;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import java.util.UUID;
 
 @Repository
 public interface DocumentPdfFileRepository extends JpaRepository<DocumentPdfFile, UUID> {
-    DocumentPdfFile findByHash(String hash);
+    DocumentPdfFile findByHash(@NonNull Integer hash);
     DocumentPdfFile findByState(String state);
+    Boolean existsByHash(@NonNull Integer hash);
 }
